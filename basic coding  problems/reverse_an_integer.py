0e190076feee82391b54346d63fc89831c9a4f66
Farhan Sharukh Hasan <sharukh75hasan@gmail.com>
2022-07-02 13:43:38 +0600

    changed a bit.

--- basic coding  problems/reverse_an_integer.py
+++ basic coding  problems/reverse_an_integer.py
@@ -1,7 +1,11 @@
 num = 6251413
 temp = num
+lst = []
 for i in range(len(str(num))):
     caps = temp // 10
     ans = temp % 10
-    print(ans)
+    #print(ans)
+    lst.append(str(ans))
     temp = caps
+ans_str = int(''.join(lst))
+print(ans_str)
